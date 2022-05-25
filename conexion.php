6229540152bca37d9dc8ce9ed2405ffda5a29b3c
Felipe Garcia <101531884+Felipegbx1330@users.noreply.github.com>
2022-05-25 16:29:55 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-	$mysqli = new mysqli("20.120.80.44","Prueba","Prueba","APP");
+	$mysqli = new mysqli("20.231.68.189","Prueba","Prueba","APP");
     if($mysqli-> connect_errno){
        echo "fallo al conectar a MySql (" . $mysqli->connect_errno . ")" . $mysqli->connect_error;
     }
