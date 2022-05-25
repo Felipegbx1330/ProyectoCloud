e29ccc5984c7713ffb1cbf565eedfad01b984a83
Felipe Garcia <101531884+Felipegbx1330@users.noreply.github.com>
2022-05-25 01:08:49 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,6 +1,8 @@
 <?php
-	$mysqli = new mysqli("10.1.0.6","PruebaApp","PruebaApp1","PruebaApp");
-    if($mysqli -> connect_errno){
+	$conexion = mysqli_int();
+	mysqli_ssl_set($conexion,NULL,NULL, "ssl/BaltimoreCyberTrustRoot.crt.pem", NULL,NULL);
+	mysqli_real_connect($conexion,"10.1.0.6","PruebaApp","PruebaApp1","PruebaApp",3306)
+    if($conexion -> connect_errno){
         echo "fallo al conectar a MySql (" . $mysqli->connect_errno . ")" . $mysqli->connect_error;
     }
 ?>
